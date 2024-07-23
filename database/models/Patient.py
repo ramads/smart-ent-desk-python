8407115b92961db23ce237858b4f4d0b76ecd56a
mausneg <maulanasuryanegara123@gmail.com>
2024-07-23 22:47:53 +0800

    change backend with open and close connection

--- database/models/Patient.py
+++ database/models/Patient.py
@@ -3,40 +3,70 @@ from database.config.config import *
 
 class PatientModel:
     def __init__(self):
+        self.db = None
+
+    def open_connection(self):
         self.db = Database(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT)
         self.db.connect()
 
+    def close_connection(self):
+        self.db.connection.close()
+
     def create_table(self):
-        query = """
-            CREATE TABLE IF NOT EXISTS Pasien (
-                id_pasien INT AUTO_INCREMENT PRIMARY KEY,
-                nama_pasien VARCHAR(255) NOT NULL,
-                jenis_kelamin ENUM('Laki-laki', 'Perempuan') NOT NULL,
-                tanggal_lahir DATE NOT NULL,
-                alamat TEXT
-            );
-        """
-        cursor = self.db.connection.cursor()
-        cursor.execute(query)
-        self.db.connection.commit()
+        try:
+            self.open_connection()
+            query = """
+                CREATE TABLE IF NOT EXISTS Pasien (
+                    id_pasien INT AUTO_INCREMENT PRIMARY KEY,
+                    nama_pasien VARCHAR(255) NOT NULL,
+                    jenis_kelamin ENUM('Laki-laki', 'Perempuan') NOT NULL,
+                    tanggal_lahir DATE NOT NULL,
+                    alamat TEXT
+                );
+            """
+            cursor = self.db.connection.cursor()
+            cursor.execute(query)
+            self.db.connection.commit()
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            self.close_connection
 
     def insert_patient(self, name, gender, birth_date, address):
-        query = """
-        INSERT INTO Pasien (nama_pasien, jenis_kelamin, tanggal_lahir, alamat)
-        VALUES (%s, %s, STR_TO_DATE(%s, '%d %M %Y'), %s);
-        """
-        cursor = self.db.connection.cursor()
-        cursor.execute(query, (name, gender, birth_date, address))
-        self.db.connection.commit()
+        try:
+            self.open_connection()
+            query = """
+            INSERT INTO Pasien (nama_pasien, jenis_kelamin, tanggal_lahir, alamat)
+            VALUES (%s, %s, STR_TO_DATE(%s, '%d %M %Y'), %s);
+            """
+            cursor = self.db.connection.cursor()
+            cursor.execute(query, (name, gender, birth_date, address))
+            self.db.connection.commit()
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            self.close_connection()
 
     def get_patient(self, patient_id):
-        query = "SELECT * FROM Pasien WHERE id_pasien = %s"
-        cursor = self.db.connection.cursor(dictionary=True)
-        cursor.execute(query, (patient_id,))
-        return cursor.fetchone()
+        try:
+            self.open_connection()
+            query = "SELECT * FROM Pasien WHERE id_pasien = %s"
+            cursor = self.db.connection.cursor(dictionary=True)
+            cursor.execute(query, (patient_id,))
+            return cursor.fetchone()
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            self.close_connection()
     
     def get_all_patients(self):
-        query = "SELECT * FROM Pasien"
-        cursor = self.db.connection.cursor(dictionary=True)
-        cursor.execute(query)
-        return cursor.fetchall()
+        try:
+            self.open_connection()
+            query = "SELECT * FROM Pasien"
+            cursor = self.db.connection.cursor(dictionary=True)
+            cursor.execute(query)
+            return cursor.fetchall()
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            self.close_connection()
