7f33f8fc2cb9e885c004e60df01790ec5a2ee907
mausneg <maulanasuryanegara123@gmail.com>
2024-07-26 02:28:48 +0800

    fix frontend for notif and notif detail, add logic, and integrate with database

--- pages/NotificationPage.py
+++ pages/NotificationPage.py
@@ -1,15 +1,21 @@
+import customtkinter
 from tkinter import *
 from colors import *
 from helpers import *
 # from notificationBar import notificationBar
 
-from pages import HomePage
+from pages import HomePage, NotificationDetailPage
+
+from database.models.Notification import NotificationModel
 
 
 class NotificationPage(Canvas, BasePage):
 
     def __init__(self, window):
         self.window = window
+        self.notification = NotificationModel()
+        self.temp_data = self.notification.get_notifications()
+        self.data_notifications = self.temp_data
         super().__init__(
             window,
             bg=BACKGROUND_COLOUR,
@@ -33,21 +39,6 @@ class NotificationPage(Canvas, BasePage):
             image=image_image_1
         )
 
-        image_image_2 = PhotoImage(
-            file=relative_to_assets("control/NotificationFrame/image_2.png"))
-        image_2 = self.create_image(
-            565.0,
-            367.3671875,
-            image=image_image_2
-        )
-
-        image_image_3 = PhotoImage(
-            file=relative_to_assets("control/NotificationFrame/image_3.png"))
-        image_3 = self.create_image(
-            565.0,
-            498.896484375,
-            image=image_image_3
-        )
 
         image_image_4 = PhotoImage(
             file=relative_to_assets("control/NotificationFrame/image_4.png"))
@@ -81,22 +72,6 @@ class NotificationPage(Canvas, BasePage):
             image=image_image_7
         )
 
-        button_image_2 = PhotoImage(
-            file=relative_to_assets("control/NotificationFrame/image_2.png"))
-        button_2 = Button(
-            image=button_image_2,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: print("button_2 clicked"),
-            relief="flat"
-        )
-        button_2.place(
-            x=960.0,
-            y=183.0,
-            width=88.0,
-            height=45.0
-        )
-
         self.create_text(
             82.0,
             183.0,
@@ -115,68 +90,101 @@ class NotificationPage(Canvas, BasePage):
             font=("Nunito Regular", 16 * -1)
         )
 
-        self.create_text(
-            75.51806640625,
-            349.444091796875,
-            anchor="nw",
-            text="Subhead",
-            fill="#9E9E9E",
-            font=("Nunito Regular", 14 * -1)
-        )
+        self.my_frame = customtkinter.CTkScrollableFrame(self.window,
+                                                         orientation="vertical",
+                                                         width=1034,
+                                                         height=320,
+                                                         fg_color=BACKGROUND_COLOUR,
+                                                         scrollbar_button_color="#bfbfbf",
+                                                         scrollbar_button_hover_color="#e0e0e0",
+                                                         scrollbar_fg_color=BACKGROUND_COLOUR,
+                                                         bg_color=BACKGROUND_COLOUR,
+                                                         label_fg_color=BACKGROUND_COLOUR,
+                                                         border_width=0)
 
-        self.create_text(
-            75.51806640625,
-            319.444091796875,
-            anchor="nw",
-            text="Weekly Maintenance",
-            fill="#404040",
-            font=("Nunito Bold", 19 * -1)
-        )
+        self.my_frame.place(x=48, y=315)
+        self.canvas_scroll = Canvas(self.my_frame, 
+                                    width=1034, 
+                                    height=2000, 
+                                    bg=BACKGROUND_COLOUR,
+                                    highlightthickness=0,
+                                    relief="ridge"
+                                    )
+        self.canvas_scroll.pack()
 
-        self.create_text(
-            75.51806640625,
-            377.444091796875,
-            anchor="nw",
-            width=980,
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Iaculis tempus tellus adipiscing eget non arcu egestas elementum faucibus. Senectus cras nunc et, arcu ultricies tristique. Mi purus ut eget euismod orci, odio eu, non. Massa sapien magna volutpat lorem. Aliquet amet elit sed ac. ",
-            fill="#404040",
-            font=("Nunito SemiBold", 14 * -1)
+        self.searchingBox = TextArea(
+            self.window,
+            bd=0,
+            bg="#FFFFFF",
+            fg="#000716",
+            highlightthickness=0,
+            font=("Nunito Bold", 12),
+            placeholder="Enter your text here..."
         )
-
-        self.create_text(
-            75.51806640625,
-            508.918212890625,
-            anchor="nw",
-            width=980,
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Iaculis tempus tellus adipiscing eget non arcu egestas elementum faucibus. Senectus cras nunc et, arcu ultricies tristique. Mi purus ut eget euismod orci, odio eu, non. Massa sapien magna volutpat lorem. Aliquet amet elit sed ac. ",
-            fill="#404040",
-            font=("Nunito SemiBold", 14 * -1)
-        )
-
-        self.create_text(
-            75.51806640625,
-            480.918212890625,
-            anchor="nw",
-            text="Subhead",
-            fill="#404040",
-            font=("Nunito Regular", 14 * -1)
-        )
-
-        self.create_text(
-            75.51806640625,
-            450.918212890625,
-            anchor="nw",
-            text="Model Update",
-            fill="#404040",
-            font=("Nunito Bold", 19 * -1)
+        self.searchingBox.place(
+            x=630.0,
+            y=195.0,
+            width=315.0,
+            height=30.0
         )
 
         inactive_button_1 = relative_to_assets("control/NotificationFrame/button_1.png")
         active_button_1 = relative_to_assets("control/NotificationFrame/active_button_1.png")
+
+        inactive_button_2 = relative_to_assets("control/NotificationFrame/button_2.png")
+        active_button_2 = relative_to_assets("control/NotificationFrame/active_button_2.png")
         
         create_hover_button(self.window, 471.0, 662.0, 192.0, 54.0, 
                             BACKGROUND_COLOUR, inactive_button_1, active_button_1, 
                             lambda: goToPage(HomePage.HomePage(self.window)))
         
+        create_hover_button(self.window, 960.0, 183.0, 88.0, 45.0, 
+                            '#FFFFFF', inactive_button_2, active_button_2, 
+                            lambda: self.searching())        
 
+
+        self.update_cards()
+
+    def searching(self):
+        input_text = self.searchingBox.get("1.0", "end-1c").lower().strip()
+        input_text = input_text.replace("\n", "").replace("\t", "").replace(" ", "")
+        print(input_text)
+        if input_text == "" or input_text == "enteryourtexthere...":
+            self.data_notifications = self.temp_data
+        else:
+            self.data_notifications = [
+                item for item in self.temp_data if input_text in item['notif_header'].lower().replace(" ", "")
+            ]
+
+        self.update_cards()
+
+
+    def update_cards(self):
+        self.canvas_scroll.delete("all")
+        card_bg = PhotoImage(file=relative_to_assets("control/NotificationFrame/image_2.png"))
+        card_read_bg = PhotoImage(file=relative_to_assets("control/NotificationFrame/image_3.png"))
+
+
+
+        for i in range(len(self.data_notifications)):
+            y_offset = i * 130
+            bg_color = "#BAED7C" if self.data_notifications[i]['already_read'] else "#ffffff"
+            card_btn = Button(self.canvas_scroll, image=card_read_bg if self.data_notifications[i]['already_read'] else card_bg, 
+                              activebackground=BACKGROUND_COLOUR, borderwidth=0, highlightthickness=0, background=BACKGROUND_COLOUR,
+                        command=lambda i=i: goToPage(NotificationDetailPage.NotificationDetailPage(self.window, self.data_notifications[i])))
+            btn_window = self.canvas_scroll.create_window(48, 307.37 + y_offset, anchor="nw", window=card_btn,
+                                            width=1034, height=121.63)
+
+            header_label = Label(self.canvas_scroll, text=self.data_notifications[i]['notif_header'], bg=bg_color, font=("Nunito Bold", 16))
+            self.canvas_scroll.create_window(75.51806640625, 319.444091796875 + y_offset, anchor="nw", window=header_label)
+
+            sub_header_label = Label(self.canvas_scroll, text=self.data_notifications[i]['notif_subheader'], bg=bg_color, font=("Nunito Regular", 10), fg="#9E9E9E")
+            self.canvas_scroll.create_window(75.51806640625, 349.444091796875 + y_offset, anchor="nw", window=sub_header_label)
+
+            content_label = Label(self.canvas_scroll, text=self.data_notifications[i]['notif_content'], bg=bg_color, font=("Nunito Regular", 12))
+            self.canvas_scroll.create_window(75.51806640625, 379.444091796875 + y_offset, anchor="nw", window=content_label)
+
+        self.canvas_scroll.configure(scrollregion=self.canvas_scroll.bbox("all"))
         self.window.mainloop()
+
+        
